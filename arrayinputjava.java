235b1babaeadf833f440ce6537652b6949992be6
Anjali853 <anjalianjali06149@gmail.com>
2025-04-19 00:32:01 +0530

    Initial commit

--- arrayinputjava.java
+++ arrayinputjava.java
@@ -1,6 +1,6 @@
 
 import java.util.*;
-public class arrayinput4java {
+public class arrayinputjava {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
         System.out.println("Enter size of arr");
