014f513cd44dea6b03681561a0a0e3e47dada5de
Anjali853 <anjalianjali06149@gmail.com>
2025-05-22 13:18:44 +0530

    try catch

---
+++ exception2.java
@@ -0,0 +1,29 @@
+// public class exception2{
+//     public static void main(String[] args) {
+//         int[] arr={2,1,2,34};
+//         try {
+//             System.out.println(arr[6]);
+            
+//         } catch (Exception e) {
+//             System.out.println(e);
+//         }
+//         System.out.println("hello");
+//     }
+// }
+
+
+public class exception2{
+    public static void main(String[] args) {
+        String str= null ;
+        System.out.println(str.toUpperCase());
+        try {
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        
+        }
+    }
+}
+
+
+
