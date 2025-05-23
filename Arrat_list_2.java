7e77f5bfd61c48f0278808ccaff45e21635bb679
Anjali853 <anjalianjali06149@gmail.com>
2025-05-23 13:11:01 +0530

    collection

---
+++ Arrat_list_2.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+public class Arrat_list_2 {
+
+    public static void main(String[] args){
+        ArrayList<String> arr=new ArrayList<>();
+    //     arr.add("anjali");
+    //     arr.add("priyanshi");
+    //     arr.add("disha");
+    //     arr.add("sneha");
+    //     arr.add("friends");
+    //     arr.set(3,"yoyo");
+    //    //gi arr.clear();
+    //     arr.remove(2);
+    arr.add("a");
+    arr.add("c");
+    arr.add("d");
+    arr.add("e");
+    Collections.sort(arr);
+        System.out.println(arr);
+
+
+
+
+        
+    
+    
+}
+}
