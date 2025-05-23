6c2fc5f6610e901aefb46f306a2ce9d58b5b8fcc
Anjali853 <anjalianjali06149@gmail.com>
2025-05-23 12:47:23 +0530

    arraylist

---
+++ arrayList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class arrayList{
+    public static void main(String[] args){
+        ArrayList<String> arr=new ArrayList<>();
+        arr.add("anjali");
+        arr.add("priyanshi");
+        arr.add("disha");
+        arr.add("sneha");
+        arr.add("friends");
+        arr.set(3,"yoyo");
+        System.out.println(arr);
+
+    }
+    
+}
