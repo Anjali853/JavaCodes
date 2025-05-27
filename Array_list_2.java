da06697eaae0f41feada3574c6544ad88dc15616
Anjali853 <anjalianjali06149@gmail.com>
2025-05-28 00:36:54 +0530

    Arraylist

--- Array_list_2.java
+++ Array_list_2.java
@@ -5,14 +5,7 @@ public class Array_list_2 {
 
     public static void main(String[] args){
         ArrayList<String> arr=new ArrayList<>();
-    //     arr.add("anjali");
-    //     arr.add("priyanshi");
-    //     arr.add("disha");
-    //     arr.add("sneha");
-    //     arr.add("friends");
-    //     arr.set(3,"yoyo");
-    //    //gi arr.clear();
-    //     arr.remove(2);
+
     arr.add("a");
     arr.add("c");
     arr.add("d");
@@ -20,11 +13,5 @@ public class Array_list_2 {
     Collections.sort(arr);
         System.out.println(arr);
 
-
-
-
-        
-    
-    
 }
 }
