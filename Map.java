6e06cf3c6da8467a0dca7d6d9d3f63f5a7800dd9
Anjali853 <anjalianjali06149@gmail.com>
2025-05-29 00:59:09 +0530

    hashmap

---
+++ Map.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Map{
+    public static void main(String[] args) {
+        HashMap<String, Integer> map=new HashMap<>();
+        map.put("id0",2);
+        map.put("id1",1);
+        map.put("id3",100);
+        System.out.print(map.get("id0"));
+    }
+    }
