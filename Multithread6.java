cbd39d8f722170f4c90cfefeb67dbb90fd1d7b94
Anjali853 <anjalianjali06149@gmail.com>
2025-06-05 12:44:01 +0530

    Multithread

---
+++ Multithread6.java
@@ -0,0 +1,13 @@
+public class Multithread6{
+    public static void main(String[] args) {
+        System.out.println("idher mat dekhooo");
+        Thread t1= new Thread(()->{  
+            System.out.println("hello");
+        }); 
+        Thread t2= new Thread(()->{
+            System.out.println("hiii");
+        });
+        t1.start();
+        t2.start();
+}
+}
