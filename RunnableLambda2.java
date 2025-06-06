949eb3294c3bee2eb1caddcceaabe8b3c0bacf0e
Anjali853 <anjalianjali06149@gmail.com>
2025-06-06 12:56:41 +0530

    RunnableLambda

---
+++ RunnableLambda2.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class RunnableLambda2{
+    public static void main(String[] args) {
+        ArrayList<String> list= new ArrayList<>();
+         list.add("anjali");
+         list.add("gazal");
+        list.add("disha");
+        // Collection.sort(list,new comparator<String>(){
+        //     public int compare(String a, String b){
+        //         return a.compareTo(b);
+        //     }
+        // });
+
+        Collections.sort(list,(a,b)->a.compareTo(b));
+        System.out.println(list);
+
+    }
+}
