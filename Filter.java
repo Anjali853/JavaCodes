e89d9f64a52bf4370b38ac1112e1a862b70c56ce
Anjali853 <anjalianjali06149@gmail.com>
2025-06-08 00:34:05 +0530

    Filter

--- Filter.java
+++ Filter.java
@@ -3,6 +3,8 @@ import java.util.*;
 import java.util.Comparator;
 import java.util.stream.Collectors;
 public class Filter{
+
+    
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         list.add(1);
@@ -10,8 +12,8 @@ public class Filter{
         list.add(6);
         list.add(9);
         list.add(2);
+       ArrayList<Integer> data= (ArrayList<Integer>) list.stream(). filter(a->a>2).collect(Collectors.toList());
 
-        ArrayList<Integer> data= (ArrayList<Integer>) list.stream(). filter(a->a>2).collect(Collectors.toList());
         System.out.println(data);
     }
 }
