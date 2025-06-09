e4bada4104fd2b9eb27640d48cdbcc986cad2329
Anjali853 <anjalianjali06149@gmail.com>
2025-06-09 12:36:06 +0530

    FilterMap

--- Filter4.java
+++ Filter4.java
@@ -4,15 +4,15 @@ import java.util.stream.Collectors;
 public class Filter4{
     public static void main(String[] args) {
         ArrayList<String> names = new ArrayList<>();
-        names.add("anjali");
-        names.add("gazal");
-        names.add("disha");
-        names.add("ayushi");
-        names.add("anushka");
+        names.add("ram");
+        names.add("shyam");
+        names.add("om");
+        names.add("sita");
         
-        ArrayList<String>filteredNames = (ArrayList<String>) names.stream().filter(name -> name.startsWith("a")).collect(Collectors.toList());
+        
+        ArrayList<String>filteredNames = (ArrayList<String>) names.stream().filter(name -> name.startsWith("s")).collect(Collectors.toList());
 
         System.out.println("Original Name:" + names);
-        System.out.println("Names starting with 'a':" + filteredNames);
+        System.out.println("Names starting with 's':" + filteredNames);
     }
 }
