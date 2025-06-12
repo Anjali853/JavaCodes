d09262d6809fd0c7ca479433b9d1759fbaadbc56
Anjali853 <anjalianjali06149@gmail.com>
2025-06-13 02:11:18 +0530

    2D array

--- Practice1.java
+++ Practice1.java
@@ -56,40 +56,40 @@ public class Practice1{
          // }
 
 
-//                    int[][] matrix = {
-//             {10, 20, 30, 40},
-//             {40, 50, 60, 70},
-//             {27, 29, 28, 37},
-//             {32, 33, 39, 66}
-//          };
+                   int[][] matrix = {
+            {10, 20, 30, 40},
+            {40, 50, 60, 70},
+            {27, 29, 28, 37},
+            {32, 33, 39, 66}
+         };
 
-//          int target = 20;
-//          int rows = matrix.length;
-//          int  cols = matrix[0].length;
+         int target = 20;
+         int rows = matrix.length;
+         int  cols = matrix[0].length;
 
-//          int i = 0, j = cols-1; 
+         int i = 0, j = cols-1; 
 
-//          boolean found = false;
-//             while (i < rows && j >= 0){
+         boolean found = false;
+            while (i < rows && j >= 0){
 
-//             if  (matrix[i][j] == target)
-//             {
+            if  (matrix[i][j] == target)
+            {
             
-//                System.out.println("Found at ("+ i + ", "+ j + ")");
+               System.out.println("Found at ("+ i + ", "+ j + ")");
 
-//                found = true;
-//                break;
-//             }else if(matrix[i][j] > target){
-//                j--;
-//             } else {
-//                i++;
+               found = true;
+               break;
+            }else if(matrix[i][j] > target){
+               j--;
+            } else {
+               i++;
       
-//              }
-//             }
-//             if(!found){
-//             System.out.println("Not Found"); 
-//         }
-//  
+             }
+            }
+            if(!found){
+            System.out.println("Not Found"); 
+        }
+ 
 
 //Spiral Traversal
 
