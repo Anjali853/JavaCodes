e2d91d8f1797cfa8280ed89dba42f6976b653ac8
Anjali853 <anjalianjali06149@gmail.com>
2025-06-13 02:08:57 +0530

    2D array

---
+++ Practice1.java
@@ -0,0 +1,103 @@
+public class Practice1{
+    public static void main(String[] args) {
+
+      // Transpose matrix
+
+//          int[][] matrix = {
+//             {1, 2, 3},
+//             {4, 5, 6}
+//          };
+//          int rows = matrix.length;
+//          int  cols = matrix[0].length;
+
+//          int [][] transpose = new int[cols][rows];
+
+//          for(int i = 0; i < rows; i++){
+//             for(int j = 0; j < cols; j++){
+
+//                 transpose[j][i] = matrix[i][j];
+//             }
+//             }
+
+//           for(int i = 0; i < cols; i++){
+//              for(int j=0; j < rows; j++){
+//                 System.out.print(transpose[i][j]+ " ");
+//              }
+//             System.out.println();
+//          }
+
+
+// 
+   
+//rotate Matrix by 90 Degrees clockwise
+
+         //    int[][] matrix = {
+         //    {1, 2, 3},
+         //    {4, 5, 6},
+         //    {7, 8, 9}
+         // };
+         // int n = matrix.length;
+   
+         // for(int i = 0; i < n; i++){
+         //    for(int j = 0; j < n; j++){
+
+         //     int tamp  = matrix[i][j];
+         //     matrix[i][j] = matrix[j][i];
+
+         //     matrix[j][i] = tamp;
+         //    }
+         //    }
+
+         //  for(int i = 0; i < n; i++){
+         //     for(int j=0; j < n; j++){
+         //        System.out.print(matrix[i][j]+ " ");
+         //     }
+         //    System.out.println();
+         // }
+
+
+//                    int[][] matrix = {
+//             {10, 20, 30, 40},
+//             {40, 50, 60, 70},
+//             {27, 29, 28, 37},
+//             {32, 33, 39, 66}
+//          };
+
+//          int target = 20;
+//          int rows = matrix.length;
+//          int  cols = matrix[0].length;
+
+//          int i = 0, j = cols-1; 
+
+//          boolean found = false;
+//             while (i < rows && j >= 0){
+
+//             if  (matrix[i][j] == target)
+//             {
+            
+//                System.out.println("Found at ("+ i + ", "+ j + ")");
+
+//                found = true;
+//                break;
+//             }else if(matrix[i][j] > target){
+//                j--;
+//             } else {
+//                i++;
+      
+//              }
+//             }
+//             if(!found){
+//             System.out.println("Not Found"); 
+//         }
+//  
+
+//Spiral Traversal
+
+
+
+}
+}
+
+
+
+
