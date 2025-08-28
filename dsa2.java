7eb701a6b946dff2bceb58976eb469c3e00926f8
Anjali853 <anjalianjali06149@gmail.com>
2025-08-28 17:32:37 +0530

    binary search

---
+++ dsa2.java
@@ -0,0 +1,35 @@
+public class dsa2 {
+    public static int binarySearch(int[] arr, int key){
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+
+            if(arr[mid] == key){
+                return mid;
+            }else if (arr[mid] < key){
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+            return -1;
+        }
+
+        public static void main(String[] args) {
+            int[] arr = {10, 20, 30, 40, 50,60};
+            int key = 40;
+
+            int result = binarySearch(arr, key);
+
+            if (result == -1) {
+                System.out.println("Element not found");
+            } else {
+                System.out.println("Element found at index: " + result);
+            }
+        }
+        
+    }
+    
+
