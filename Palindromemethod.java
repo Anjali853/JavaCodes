409dd4b8cf69dd204b41d8e13219ab3036e456af
Anjali853 <anjalianjali06149@gmail.com>
2025-09-18 23:49:52 +0530

    palinderomemethod

---
+++ Palindromemethod.java
@@ -0,0 +1,21 @@
+public class Palindromemethod {
+    public boolean isPalindrome(int x){
+        if (x < 0 ||(x % 10 == 0 && x != 0)) return false;
+
+        int reversed = 0;
+        while (x > reversed) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
+            
+        }
+
+        return x == reversed || x == reversed / 10;
+    }
+    public static void main(String[] args) {
+        Palindromemethod sol = new Palindromemethod();
+        System.out.println(sol.isPalindrome(121));
+        System.out.println(sol.isPalindrome(-121));
+        System.out.println(sol.isPalindrome(10));
+        System.out.println(sol.isPalindrome(12321));
+    }
+}
