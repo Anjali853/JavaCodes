4a41df40b95f4f2f81f7aae7ba044e585273b009
Anjali853 <anjalianjali06149@gmail.com>
2025-09-22 23:53:53 +0530

    requencysum

--- frequency.java
+++ frequency.java
@@ -1,36 +1,35 @@
 import java.util.*;
 
 public class frequency {
-    //public static void main(String args[]){
+    public static void main(String args[]){
  
-    //  String str="programming with rupesh sir";
-    //     HashMap<Character, Integer> map= new HashMap<>();
-    //     for(char ch:str.toCharArray() ){
-    //         map.put(ch, map.getOrDefault(ch,0)+1);
-    //     }
-    //     System.out.println(map);
-    //     char[] ch=str.toCharArray();    
-    // }
+     String str="programming with rupesh sir";
+        HashMap<Character, Integer> map= new HashMap<>();
+        for(char ch:str.toCharArray() ){
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        System.out.println(map);
+        char[] ch=str.toCharArray();    
+    }
 
- //   String str="siisisirji";
-   // String[] newstr=str.split(" ");
-       // HashMap<String, Integer> map= new HashMap<>();
-    //     for(String ch:newstr ){
-    //         map.put(ch, map.getOrDefault(ch,0)+1);
-    //     }
-    //     System.out.println(map);
-    //     char[] ch=str.toCharArray();    
-    // }
+//    String str="siisisirji";
+//    String[] newstr=str.split(" ");
+//        HashMap<String, Integer> map= new HashMap<>();
+//         for(String ch:newstr ){
+//             map.put(ch, map.getOrDefault(ch,0)+1);
+//         }
+//         System.out.println(map);
+//         char[] ch=str.toCharArray();    
+//     }
 
-    public static void main(String[] args) {
-         String str = "aabbb";
-        HashSet<String> set = new HashSet<>();
-        for (Str i = 0; i <str.toCharArray() ; i++) {
-            if (set.contains(i)) {
-                System.out.println(str[i]);
-            } else {
-                set.add(String[i]);
+//     public static void main(String[] args) {
+//          String str = "aabbb";
+//         HashSet<String> set = new HashSet<>();
+//         for (Str i = 0; i <str.toCharArray() ; i++) {
+//             if (set.contains(i)) {
+//                 System.out.println(str[i]);
+//             } else {
+//                 set.add(String[i]);
         
-                }    }
-}
-}
+//                 }    }
+ }
