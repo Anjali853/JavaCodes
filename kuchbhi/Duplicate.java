8f82b1da2c96702caef8108bea14afb4b029f7e8
Anjali853 <anjalianjali06149@gmail.com>
2025-12-11 20:50:19 +0530

    duplicate

---
+++ kuchbhi/Duplicate.java
@@ -0,0 +1,21 @@
+package kuchbhi;
+
+public class Duplicate { 
+    public static void main(String[] args) {
+        
+    
+    int[] arr = {21,35,35,47,47,56,68};
+    int j = 0;
+    for (int i = 1; i <arr.length; i++){
+       if (arr[i] != arr[j]){
+        j++;
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+       }
+    }
+    for (int i = 0; i<=j; i++){
+        System.out.println(arr[i] + " ");
+    }
+}
+}
