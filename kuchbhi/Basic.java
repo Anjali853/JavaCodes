688ff5b16af8ab3d9fd9cc333f9cae553618d5f4
Anjali853 <anjalianjali06149@gmail.com>
2025-12-11 21:04:49 +0530

    backto basic

---
+++ kuchbhi/Basic.java
@@ -0,0 +1,27 @@
+package kuchbhi;
+
+public class Basic {
+    public static void main(String[] args) {
+        //reverse
+        //  String str = "Automation";
+        //  StringBuilder reversed = new StringBuilder(str).reverse();
+        //  System.out.println(reversed);
+
+
+        //palindrom
+        // String str = "madam";
+        // String reversed = new StringBuilder(str).reverse().toString();
+        // System.out.println(str.equals(reversed));
+
+
+        //fibonacci series
+        // int n = 10, num1 = 0, num2 = 1;
+        // System.out.println("Fibonacci Series:" + num1 + "," + num2);
+        // for (int i = 2; i<n; i++){
+        //     int num3 = num1 + num2;
+        //     System.err.println("" + num3);
+        //     num1 = num2; num2 = num3;
+       // }
+    }
+    
+}
