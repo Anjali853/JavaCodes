cc096ff33378514ac0498d15e8bb94d577ff5c4d
Anjali853 <anjalianjali06149@gmail.com>
2025-12-26 15:54:11 +0530

    InsertionSort

--- kuchbhi/Basic.java
+++ kuchbhi/Basic.java
@@ -1,5 +1,7 @@
 package kuchbhi;
 
+import java.util.Scanner;
+
 public class Basic {
     public static void main(String[] args) {
         //reverse
@@ -24,19 +26,87 @@ public class Basic {
        // }
 
 
-       String s = "All izz well  ";
-       int count = 0;
-       for (int i = s.length()-1; i>=0; i--){
-        if(s.charAt(i) != ' ')
-        {
-            count++;
-        }
-        else if (s.charAt(i)== ' ' && count != 0){
-            break;
-        }
-        System.out.println("Lenth of last word is " + count);
-       }
+    //    String s = "All izz well  ";
+    //    int count = 0;
+    //    for (int i = s.length()-1; i>=0; i--){
+    //     if(s.charAt(i) != ' ')
+    //     {
+    //         count++;
+    //     }
+    //     else if (s.charAt(i)== ' ' && count != 0){
+    //         break;
+    //     }
+    //     System.out.println("Lenth of last word is " + count);
+    //    }
 
-    }
+
+
+    // Scanner sc = new Scanner(System.in);
+    // System.out.println("Enter roll No");
+    // int rollNo = sc.nextInt();
+    // System.out.println("Enter name");
+    // String name = sc.next();
+    // System.out.println("Enterfee");
+    // Double fee = sc.nextDouble();
+    // sc.close();
+
+
+
+    // int  a = 15;
+    // int b =  30;
+    // int sum;
+    // sum = a+b ;
+    // System.out.println("SUM" + sum);
+
+    // String name = "anjali";
+    // int age = 20;
+    // String clg = "global";
+    // boolean isgood = true;
+    // System.out.println("my name is " + name + " my age " + age + " clg " + clg + " isgood " + isgood );
+// for (int i = 1; i <= 5; i++){
+//     System.out.println(i);
+// }
+
+// int i = 0;
+// while (i <=5) {
+//     System.out.println(i);
+//     i++;
+// }
+
+// int i =1;
+// do{
+//     System.out.println(i);
+//     i++;
+// }while (i <= 5);
+
+// for (int i = 1; i <= 10; i++){
+//     if (i % 2 == 0){
+//         System.out.println(i);
+//     }
+// }
+
+// for (int i = 1; i <= 10; i++){
+//     if (i == 5) break;
+//     System.out.println(i);
+// }
+
+// for (int i = 1; i <= 10; i++){
+//     System.out.println("2 x " + i + " = " + (2 * i));
+// }
+
+// int sum =0;
+// for (int i = 1; i <= 10; i++){
+//     sum = sum + i;
+// }
+// System.out.println("Sum = " + sum);
+int i = 0;
+int sum = 0;
+while (i <= 10) {
+    sum += i;
+    i++;
+    
+}
+System.out.println(sum);
+   }
     
 }
